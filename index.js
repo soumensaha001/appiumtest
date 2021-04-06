57833518a42837069ee9accf08522988e49d14bc
Soumen Saha <45451838+soumensaha001@users.noreply.github.com>
2021-04-06 13:16:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// javascript
+
+const wdio = require("webdriverio");
+const assert = require("assert");
+
+const opts = {
+  path: '/wd/hub',
+  port: 4723,
+  capabilities: {
+    platformName: "Android",
+    deviceName: "emulator-5554",
+    app: "/root/tmp/ApiDemos-debug.apk",
+    appPackage: "io.appium.android.apis",
+    appActivity: ".view.TextFields",
+    automationName: "UiAutomator2"
+  }
+};
+
+async function main () {
+  const client = await wdio.remote(opts);
+
+  const field = await client.$("android.widget.EditText");
+  await field.setValue("Hello World!");
+  const value = await field.getText();
+  assert.strictEqual(value,"Hello World!");
+
+  await client.deleteSession();
+}
+
+main();
